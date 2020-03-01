040fb737b5a0b8cd2404e3c5c53b1f6f9b0d851b
PeterPPark <petepp@naver.com>
2020-03-02 00:56:02 +0900

    remain error in chrome

--- README.md
+++ README.md
@@ -1,31 +1,28 @@
-<<<<<<< HEAD
 # coronamap
 
-> A Vue.js project
-
-## Build Setup
-
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-```
-
-For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader)
 
 -----
 
 [코로나인포_api](https://api.coronas.info/)
+[코로나인포](https://coronas.info/)
 
-<h3>코로나인포 api를 사용합니다.</h3>
+>코로나인포 OpenAPI를 사용합니다
+
+### 💻 수정사항들
+
+### 2020-02-29
+```md
+- axios를 통한 vue 연동  
+- 카카오맵 수정사항 필요
+```  
+
+### 2020-03-01
+
+```md
+-2020.03.01: chrome error 수정 필요
+```
+-----
 
 
--2020.02.28: axios를 통한 vue 연동
 
-=======
 
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <div id="app">
     <h2>contagious map</h2>
-    <div class="place">
-    <li v-for="position of positions" :key=position.id>
-          {{position.lat}}
-    </li>
     </div>
-  </div>
 </template>
 
 <script>
@@ -14,21 +9,55 @@ import axios from 'axios'
 export default{
 data:()=>{
   return{
-  positions:[]
+    positions:[],
+    map:[]
 }
 },
 created(){
 axios.get('https://api.coronas.info/movements/')
     .then(response=>{
-    const core=response.data.map(val=>({lat:val.lat,lng:val.lng,patient:val.patient}));
-    for(let i=0;i<core.length;i++){
-      let placelist= core[i];
-      positions.push(placelist);
+      this.map=response.data.map(val=>({
+      title:val.patient,
+      lat:val.lat,
+      lng:val.lng
+      }));
+      for(let i=0;i<this.map.length;i++){
+      let tmp = this.map[i];
+      let position = {lat:tmp.lat,lng:tmp.lng};
+      this.positions.push(position);
     }
-    })
-    .catch(e=>{
-      console.log('error:',e)
-    })
+      }
+    )
 },
+mounted(){
+ var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
+          mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 14 // 지도의 확대 레벨
+        };
+      
+        var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+        var imageSrc = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+
+        for (var i = 0; i < this.map.length; i ++) {
+    
+        // 마커 이미지의 이미지 크기 입니다
+        var imageSize = new kakao.maps.Size(24, 35); 
+        
+        // 마커 이미지를 생성합니다    
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+        
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: this.positions[i], // 마커를 표시할 위치
+            title : this.map[i].patient, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image : markerImage // 마커 이미지 
+        });
+  }
 }
+}
+
 </script>
+
