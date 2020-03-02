e089370b97e6e044e2d30e5f6b50735f72d6d7f1
PeterPPark <48914806+PeterPPark@users.noreply.github.com>
2020-03-02 14:36:45 +0900

    revise app.vue

--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,7 @@ mounted(){
         var marker = new kakao.maps.Marker({
             map: map, // 마커를 표시할 지도
             position: this.positions[i], // 마커를 표시할 위치
-            title : this.map[i].patient, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            title : this.map[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
             image : markerImage // 마커 이미지 
         });
   }
